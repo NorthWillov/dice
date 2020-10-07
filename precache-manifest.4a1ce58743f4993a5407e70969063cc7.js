9bfc9cf8b5595c35930e717ca6da0b1e63f3b3d5
Artem <artem@artemsMac.local>
2020-10-07 12:57:30 +0200

    Updates

---
+++ precache-manifest.4a1ce58743f4993a5407e70969063cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60826d248ee5410d84ff8a99dc25dcd7",
+    "url": "/dice/index.html"
+  },
+  {
+    "revision": "94e9129f910067750c85",
+    "url": "/dice/static/css/main.928e9083.chunk.css"
+  },
+  {
+    "revision": "3bf8bc923fb905838459",
+    "url": "/dice/static/js/2.fe1fb92b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice/static/js/2.fe1fb92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e9129f910067750c85",
+    "url": "/dice/static/js/main.deb3459f.chunk.js"
+  },
+  {
+    "revision": "6f349f4a9e93576960a4",
+    "url": "/dice/static/js/runtime-main.6581301c.js"
+  }
+]);
